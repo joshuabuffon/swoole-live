7faef01783d5df49771b7b32d7095ce437543025
joshuabuffon <joshuabuffon@163.com>
2018-04-15 12:06:37 +0800

    添加http_server
    添加http_server

---
+++ thinkphp/server/http_server.php
@@ -0,0 +1,18 @@
+<?php
+
+$http = new swoole_http_server("0.0.0.0", 8811);
+
+$http->set(
+    [
+        'enable_static_handler' => true,
+        'document_root' => "/home/www/wwwroot/swoole-live/thinkphp/public/static",
+    ]
+);
+
+$http->on('request', function($request, $response) {
+
+    $response->cookie("singwa", "xsssss", time() + 1800);
+    $response->end("sss". json_encode($request->get));
+});
+
+$http->start();
